7cde3b6119d7e11b62e5ae34d4bd40a8332b81c1
memesmith67 <reddit.moderator.wizard@gmail.com>
2026-08-11 01:21:23 -0700

    Update brainspungus.fastspungus.c

--- brainspungus.fastspungus.c
+++ brainspungus.fastspungus.c
@@ -4,24 +4,24 @@ declare int j
 declare int* n
 declare int a
 declare int b
-assign j *(int*)m + 1
+assign j **(int**)m + 1
 assign n n + j
 assign j *n
 assign n n + j
 declare int a
 assign a *n
-assign j *(int*)m + 2
+assign j **(int**)m + 2
 assign n n + j
 assign j *n
 assign n n + j
 declare int b
 assign b *n
-assign n (int*)m + *(int*)m
+assign n *(int**)m + **(int**)m
 assign j *n
 switch j
 case 0 b
-assign j *(int*)m + 2
-assign n (int*)m + j
+assign j **(int**)m + 2
+assign n *(int**)m + j
 assign a *n
 break
 case 1 -
@@ -31,20 +31,33 @@ case 2 <
 assign a a < b
 break
 case 3 m
-assign n (int*)m + b
-assign j *(int*)m
-assign n (int*)m + a
+assign n *(int**)m + b
+assign j **(int**)m
+assign n *(int**)m + a
 assign * j
 break
 case 4 x
-assign *(int*)m a - 3
+assign **(int**)m a - 3
 break
+case 5 a
 fastspungus
-case 'a':n=realloc(m,8*a)?m=n:a=0;break;
+n=realloc(m,a);
 fastspungus
+assign j n == null
+switch j
+case 0
+declare int* o
+assign o *m + a
+assign *o 0
+break
+default:
+assign *m n
+break
 }
-assign j *(int*)m + 3
-assign *(int*)m j
+break
+}
+assign j **(int**)m + 3
+assign **(int**)m j
 }
 }
 fastspungus
