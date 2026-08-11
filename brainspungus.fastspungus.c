ecb6d71ec9612dd4b6119558971454c659c66637
memesmith67 <reddit.moderator.wizard@gmail.com>
2026-08-10 21:55:29 -0700

    Create brainspungus.fastspungus.c

---
+++ brainspungus.fastspungus.c
@@ -0,0 +1,50 @@
+fastspungus
+function brainspungus m copyright john morris beck 2026 gpl2
+declare int j
+declare int* n
+declare int a
+declare int b
+assign j *(int*)m + 1
+assign n n + j
+assign j *n
+assign n n + j
+declare int a
+assign a *n
+assign j *(int*)m + 2
+assign n n + j
+assign j *n
+assign n n + j
+declare int b
+assign b *n
+assign n (int*)m + *(int*)m
+assign j *n
+switch j
+case 0 b
+assign j *(int*)m + 2
+assign n (int*)m + j
+assign a *n
+break
+case 1 -
+assign a a - b
+break
+case 2 <
+assign a a < b
+break
+case 3 m
+assign n (int*)m + b
+assign j *(int*)m
+assign n (int*)m + a
+assign * j
+break
+case 4 x
+assign *(int*)m a - 3
+break
+fastspungus
+case 'a':n=realloc(m,8*a)?m=n:a=0;break;
+fastspungus
+}
+assign j *(int*)m + 3
+assign *(int*)m j
+}
+}
+fastspungus
