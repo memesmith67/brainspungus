5ac448d3d94d48c241df271a9a6229e2623107a3
memesmith67 <reddit.moderator.wizard@gmail.com>
2026-08-12 14:12:47 -0700

    Update brainspungus.fastspungus.c

--- brainspungus.fastspungus.c
+++ brainspungus.fastspungus.c
@@ -1,6 +1,6 @@
 //copyright john morris beck 2026 gpl2
 fastspungus
-function brainspungus m
+function int* brainspungus int* m
 declare int* p0
 declare int* p1
 declare int i0
@@ -12,51 +12,51 @@ declare int t
 declare int* n
 declare int j
 declare int k
-get t int* m
-assign p0 int* m + int t
-get i0 int* p0
-assign p0 int* p0 + int 1
-get i1 int* p0
-assign p0 int* p0 + int 1
-get i2 int* p0
-assign r0 int* m + int i0
-assign r1 int* m + int i1
-switch int i2
-case int 0
-get j int* r0
-assign n int* m + int j
-get j int* r1
-set n int j
+get t m
+assign p0 m + t
+get i0 p0
+assign p0 p0 + 1
+get i1 p0
+assign p0 p0 + 1
+get i2 p0
+assign r0 m + i0
+assign r1 m + i1
+switch i2
+case 0
+get j r0
+assign n m + j
+get j r1
+set n j
 break;
-case int 1
-get j int* r0
-get k int* r1
-assign j int j - int k
-set r0 int j
+case 1
+get j r0
+get k r1
+assign j j - k
+set r0 j
 break;
-case int 2
-get j int* r0
-get k int* r1
-assign j int j < int k
-set r0 int j
+case 2
+get j r0
+get k r1
+assign j j < k
+set r0 j
 break;
-case int 3
-get j int* r0
-get k int* r1
-assign p0 int* m + int j
-assign p1 int* m + int k
-get j int* p1
-set p0 int j
+case 3
+get j r0
+get k r1
+assign p0 m + j
+assign p1 m + k
+get j p1
+set p0 j
 break;
-case int 4
-get j int* r0
-assign j int j + int 3
-set m int j
+case 4
+get j r0
+assign j j - 3
+set m j
 break;
 }
-get j int* m
-assign j int j + int 3
-set m int j
+get j m
+assign j j + 3
+set m j
 declare return m
 }
 fastspungus
